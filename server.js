e6645889bbcfac08a0c0d56cac95d0126d1bbaa5
Owen Bringino <owenrb@Owens-MacBook-Pro.local>
2021-09-26 16:25:31 +0800

    fix heroku deployment

---
+++ server.js
@@ -0,0 +1,23 @@
+// Redirect all traffic from http to https.
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+// Serve our static files.
+app.use(express.static('./dist/profile'));
+
+// Wait for a request to any path and redirect all of the requests to index.html.
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/profile' }
+    );
+});
+
+// Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
+app.listen(process.env.PORT || 8080);
